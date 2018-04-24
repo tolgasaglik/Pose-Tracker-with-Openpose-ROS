812d526adc7ac17db90b5308e18328a730fb4736
tolgasaglik <tolgasaglik@gmail.com>
2018-04-24 19:39:42 +0200

    Publisher multiple people enabled.

--- src/gesture_listener.py
+++ src/gesture_listener.py
@@ -26,22 +26,31 @@ C_RIGHT_EAR = 16
 C_LEFT_EAR = 17
 C_BACKGROUND = 18
 
-
 # GESTURE CONSTANTS
 G_HAND_RAISE = 0
 G_WAIT_A_MINUTE = 1
 G_BREAK_ACTIVITY = 2
 G_GO_RIGHT = 3
 G_GO_LEFT = 4
+G_REACH_UP = 5
+G_BEND_RIGHT = 6
+G_BEND_LEFT = 7
+
+# GESTURE CONSTANTS STRINGS
+GS_HAND_RAISE = "HAND"
+GS_WAIT_A_MINUTE = "STOP"
+GS_BREAK_ACTIVITY = "BREAK"
+GS_GO_RIGHT = "RIGHT"
+GS_GO_LEFT = "LEFT"
+GS_REACH_UP = "REACH_UP"
+GS_BEND_RIGHT = "BEND_RIGHT"
+GS_BEND_LEFT = "BEND_LEFT"
 
 
 class Gesture_Detector(object):
     """Gesture Detection Node on ROS using Openpose"""
-
-    # publisher_msg = GestureDetectorHumanList
-    # gesture_list = GestureDetectorHuman()
-
     def __init__(self):
+        self.pub = rospy.Publisher("/gesture_detector/gesture_list", GestureDetectorHumanList, queue_size=10)
         self.publisher_msg = GestureDetectorHumanList()
         """Initialize ROS components"""
         self.init_node()
@@ -52,7 +61,6 @@ class Gesture_Detector(object):
         self.gesture_detector()
 
     """Gesture Recognizers"""
-
     def hand_raise(self, human):
         return (((human.body_key_points_with_prob[C_LEFT_SHOULDER].y > human.body_key_points_with_prob[C_LEFT_WRIST].y > 0)
                  or (human.body_key_points_with_prob[C_RIGHT_SHOULDER].y > human.body_key_points_with_prob[C_RIGHT_WRIST].y > 0))
@@ -60,9 +68,10 @@ class Gesture_Detector(object):
                          and (human.body_key_points_with_prob[C_RIGHT_SHOULDER].y > human.body_key_points_with_prob[C_RIGHT_WRIST].y > 0)))
 
     def wait_a_minute(self, human):
-        return ((human.body_key_points_with_prob[C_RIGHT_SHOULDER].y > human.body_key_points_with_prob[C_RIGHT_WRIST].y > 0)
+        return ((human.body_key_points_with_prob[C_RIGHT_ELBOW].y > human.body_key_points_with_prob[C_RIGHT_SHOULDER].y > human.body_key_points_with_prob[C_RIGHT_WRIST].y > 0)
                 and (human.body_key_points_with_prob[C_LEFT_WRIST].x > human.body_key_points_with_prob[C_NECK].x > human.body_key_points_with_prob[C_RIGHT_WRIST].x)
-                and (human.body_key_points_with_prob[C_LEFT_SHOULDER].y > human.body_key_points_with_prob[C_LEFT_WRIST].y > 0))
+                and (human.body_key_points_with_prob[C_LEFT_ELBOW].y > human.body_key_points_with_prob[C_LEFT_SHOULDER].y > human.body_key_points_with_prob[C_LEFT_WRIST].y > 0)
+                and (human.body_key_points_with_prob[C_LEFT_WRIST].x - human.body_key_points_with_prob[C_RIGHT_WRIST].x) > (human.body_key_points_with_prob[C_LEFT_SHOULDER].x - human.body_key_points_with_prob[C_RIGHT_SHOULDER].x))
 
     def break_activity(self, human):
         return ((human.body_key_points_with_prob[C_LEFT_SHOULDER].x > human.body_key_points_with_prob[C_LEFT_WRIST].x > human.body_key_points_with_prob[C_RIGHT_SHOULDER].x > 0)
@@ -76,70 +85,93 @@ class Gesture_Detector(object):
                 and (0 < human.body_key_points_with_prob[C_LEFT_SHOULDER].y < human.body_key_points_with_prob[C_LEFT_WRIST].y)
                 and (0 < human.body_key_points_with_prob[C_RIGHT_SHOULDER].y < human.body_key_points_with_prob[C_RIGHT_WRIST].y))
 
-    def go_leftt(self, human):
+    def go_left(self, human):
         return ((0 < human.body_key_points_with_prob[C_LEFT_WRIST].x < human.body_key_points_with_prob[C_NECK].x)
                 and (0 < human.body_key_points_with_prob[C_RIGHT_WRIST].x < human.body_key_points_with_prob[C_RIGHT_ELBOW].x < human.body_key_points_with_prob[C_RIGHT_SHOULDER].x)
                 and (0 < human.body_key_points_with_prob[C_LEFT_SHOULDER].y < human.body_key_points_with_prob[C_LEFT_WRIST].y)
                 and (0 < human.body_key_points_with_prob[C_RIGHT_SHOULDER].y < human.body_key_points_with_prob[C_RIGHT_WRIST].y))
 
-    """Operational Functions"""
+    def reach_up(self, human):
+        return (0 < human.body_key_points_with_prob[C_RIGHT_WRIST].y < human.body_key_points_with_prob[C_RIGHT_ELBOW].y < human.body_key_points_with_prob[C_RIGHT_SHOULDER].y
+                and 0 < human.body_key_points_with_prob[C_LEFT_WRIST].y < human.body_key_points_with_prob[C_LEFT_ELBOW].y < human.body_key_points_with_prob[C_LEFT_SHOULDER].y
+                and (human.body_key_points_with_prob[C_LEFT_WRIST].x - human.body_key_points_with_prob[C_RIGHT_WRIST].x) < 1.3 * (human.body_key_points_with_prob[C_LEFT_SHOULDER].x - human.body_key_points_with_prob[C_RIGHT_SHOULDER].x))
 
+    def lateral_left_bend(self, human):
+        return (0 < human.body_key_points_with_prob[C_RIGHT_SHOULDER].y < human.body_key_points_with_prob[C_NECK].y < human.body_key_points_with_prob[C_LEFT_SHOULDER].y
+                and 0 < human.body_key_points_with_prob[C_RIGHT_HIP].x < human.body_key_points_with_prob[C_RIGHT_SHOULDER].x
+                and 0 < human.body_key_points_with_prob[C_LEFT_HIP].x < human.body_key_points_with_prob[C_NECK].x)
+
+    def lateral_right_bend(self, human):
+        return (0 < human.body_key_points_with_prob[C_LEFT_SHOULDER].y < human.body_key_points_with_prob[C_NECK].y < human.body_key_points_with_prob[C_RIGHT_SHOULDER].y
+                and 0 < human.body_key_points_with_prob[C_LEFT_SHOULDER].x < human.body_key_points_with_prob[C_LEFT_HIP].x
+                and 0 < human.body_key_points_with_prob[C_NECK].x < human.body_key_points_with_prob[C_RIGHT_HIP].x)
+    """Gesture recognizers end"""
+
+    """Operational Functions """
     def callback(self, msg):
         self.publisher_msg.num_humans = len(msg.human_list)
-        for human in msg.human_list:  # type: object
-            ind = msg.human_list.index(human)  # type: object
-            rospy.loginfo("Gesture from Human: " + str(ind))
-            self.publisher_msg.num_humans = len(msg.human_list)
-            self.publisher_msg.gesture_list = [GestureDetectorHuman()] * (ind + 1)
-            self.publisher_msg.gesture_list[msg.human_list.index(human)].human_index = msg.human_list.index(human)
+
+        # self.publisher_msg.gesture_list = [GestureDetectorHuman()] * len(msg.human_list)
+        # rospy.loginfo("Visible humans:" + str(human_list_length))
+        i = 0
+        detected_human_list = GestureDetectorHumanList()
+        detected_human_list.num_humans = len(msg.human_list)
+
+        for human in msg.human_list:
+
+            detected_human = GestureDetectorHuman()
+            detected_human.human_index = i
+            rospy.loginfo("Gesture from Human: " + str(i))
 
             # Check gesture published by current human and publish respective message
             if self.hand_raise(human):
-                self.publisher_msg.gesture_list[msg.human_list.index(human)].gesture = str(G_HAND_RAISE)
-                rospy.loginfo("Human:" + str(ind) + " is raising a HAND!")
-            elif self.wait_a_minute(human):
-                self.publisher_msg.gesture_list[msg.human_list.index(human)].gesture = str(G_WAIT_A_MINUTE)
-                rospy.loginfo("Human:" + str(ind) + " tells PEPPER to wait!")
-            elif self.break_activity(human):
-                self.publisher_msg.gesture_list[msg.human_list.index(human)].gesture = str(G_BREAK_ACTIVITY)
-                rospy.loginfo("Human:" + str(ind) + " tells PEPPER to break activity!")
-            elif self.go_right(human):
-                self.publisher_msg.gesture_list[msg.human_list.index(human)].gesture = str(G_GO_RIGHT)
-                rospy.loginfo("Human:" + str(ind) + " tells PEPPER to go right!")
-            elif self.go_left(human):
-                self.publisher_msg.gesture_list[msg.human_list.index(human)].gesture = str(G_GO_LEFT)
-                rospy.loginfo("Human:" + str(ind) + " tells PEPPER to go left!")
-            rospy.loginfo("\n")
-            #self.publisher_msg.gesture_list[0].gesture = "home"
-            # self.publisher_msg.gesture_list[1]= (ind, str(G_HAND_RAISE))
+                # rospy.loginfo("Human:" + str(ind) + " is raising a HAND!")
+                detected_human.gesture = str(GS_HAND_RAISE)
 
-            # for key_point in human.body_key_points_with_prob[2:8]:
-            #     x = key_point.x
-            #     y = key_point.y
-            #     prob = key_point.prob
-            #     rospy.loginfo('x: {}, y: {}, prob: {}'.format(x, y, prob))
-            # if self.hand_raise(human):
-            #     rospy.loginfo("Human:" + str(ind) + " is raising a HAND!")
-            #     self.publisher_msg.gesture_list.append((ind, str(G_HAND_RAISE)))
-            #     self.pub.publish(self.publisher_msg)
-            # elif self.wait_a_minute(human):
-            #     rospy.loginfo("Human:" + str(ind) + " tells PEPPER to wait!")
-            # elif self.break_activity(human):
-            #     rospy.loginfo("Human:" + str(ind) + " tells PEPPER to break activity!")
-            # rospy.loginfo("\n")
+            elif self.wait_a_minute(human):
+                # rospy.loginfo("Human:" + str(ind) + " tells PEPPER to wait!")
+                detected_human.gesture = str(GS_WAIT_A_MINUTE)
+
+            elif self.break_activity(human):
+                # rospy.loginfo("Human:" + str(ind) + " tells PEPPER to break activity!")
+                detected_human.gesture = str(GS_BREAK_ACTIVITY)
+
+            elif self.go_right(human):
+                # rospy.loginfo("Human:" + str(ind) + " tells PEPPER to go right!")
+                detected_human.gesture = str(GS_GO_RIGHT)
+
+            elif self.go_left(human):
+                # rospy.loginfo("Human:" + str(ind) + " tells PEPPER to go left!")
+                detected_human.gesture = str(GS_GO_LEFT)
+
+            elif self.reach_up(human):
+                # rospy.loginfo("Human:" + str(ind) + " is reaching reach up!")
+                detected_human.gesture = str(GS_REACH_UP)
+
+            elif self.lateral_right_bend(human):
+                # rospy.loginfo("Human:" + str(ind) + " is bending right!")
+                detected_human.gesture = str(GS_BEND_RIGHT)
+
+            elif self.lateral_left_bend(human):
+                # rospy.loginfo("Human:" + str(ind) + " is bending left!")
+                detected_human.gesture = str(GS_BEND_RIGHT)
+
+            detected_human_list.gesture_list.append(detected_human)
+            i += 1
+            rospy.loginfo("\n")
+            rospy.loginfo("--" + detected_human.gesture)
+            # rospy.loginfo(detected_human_list)
+            rospy.loginfo("\n")
+            # rospy.loginfo(self.publisher_msg.gesture_list)
+
+        self.pub.publish(detected_human_list)
 
     def gesture_detector(self):
         rospy.init_node("gesture_detector", anonymous=True)
-        self.pub = rospy.Publisher("/gesture_detector/gesture_list", GestureDetectorHumanList, queue_size=10)
         rospy.Subscriber("/openpose_ros/human_list", OpenPoseHumanList, self.callback)
-        r = rospy.Rate(1)
+        r = rospy.Rate(2)
 
-        # publisher_msg = GestureDetectorHumanList()
-        # gesture_list = GestureDetectorHuman()
         while not rospy.is_shutdown():
-            # publish should stay in this loop to terminate the algorithm when CTRL-C is pressed
-            # o/w terminal should be manually shut down
-            self.pub.publish(self.publisher_msg)
             r.sleep()  # sleep for one second
         # rospy.spin()
 
